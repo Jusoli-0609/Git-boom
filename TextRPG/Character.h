6f32b007b383eb0298bd519a14a6f4b78488319f
박성빈 <anarchistmay@gmail.com>
2026-07-29 23:10:26 +0900

    삭제완료

--- TextRPG/Character.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-
-#include <string>
-
-const int SIZE = 4;
-
-void PrintStatus(const std::string& name, int stat[]);
-void CharacterCreation(std::string& name, int stat[]);
--- TextRPG/JobSelect.h
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-
-#include <string>
-#include "Player.h"
-
-Player* JobSelect(const std::string & name, int stat[]);
